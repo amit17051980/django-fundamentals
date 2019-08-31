67de39601d265bd9e91137ceda57570562490523
administrator <administrator@administrator-Virtual-Machine>
2019-08-31 13:05:59 +0100

    Added graphviz ubuntu package

---
+++ LMS/LMS/Setup-Ubuntu-Pkg.sh
@@ -0,0 +1,8 @@
+#bash
+sudo apt update -y
+sudo apt install -y python3.7
+sudo apt install -y python3-pip
+sudo apt install -y python3-venv
+sudo apt install -y xmlsec1
+sudo apt-get install graphviz
+
